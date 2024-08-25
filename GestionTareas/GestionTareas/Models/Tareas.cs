980501293c7766b7e24275eb69ac6b4c767f54b6
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-08-25 11:39:10 -0400

    Realice el modelo

---
+++ GestionTareas/GestionTareas/Models/Tareas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionTareas.Models;
+
+public class Tareas
+{
+    [Key]
+    public int TareaId { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Priority { get; set; }
+    public string? Status { get; set; }
+
+}
