3b77ba01fe0bc7c9d41e89649ca60173ae4c00f4
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-08-25 11:38:53 -0400

    Realice el contexto

---
+++ GestionTareas/GestionTareas/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using GestionTareas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GestionTareas.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> optinos) : base(optinos) { }
+    public DbSet<Tareas> Tareas { get; set; }
+}
