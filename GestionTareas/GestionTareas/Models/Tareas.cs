dd6fe3757378173b1f42fbe932f66dce2c6d1da2
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-08-27 09:56:11 -0400

    Realice la validacion

--- GestionTareas/GestionTareas/Models/Tareas.cs
+++ GestionTareas/GestionTareas/Models/Tareas.cs
@@ -6,9 +6,21 @@ public class Tareas
 {
     [Key]
     public int TareaId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Title { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Description { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Priority { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Status { get; set; }
 
 }
